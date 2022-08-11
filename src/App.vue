7aceed1d7c1355e165893b6dea076af22318833e
changyi <changyi@biomap.com>
2022-08-11 17:32:23 +0800

    feat: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Button></Button>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Button from "./components/Button.tsx";
+</script>
+
+<style scoped></style>
